a0c5e354ef35a140365e867eaef3dee05829d43d
bgb0923 <bgb0923@gmail.com>
2017-02-08 23:42:08 -0500

    Followed along with lesson

--- Structs.playground/Contents.swift
+++ Structs.playground/Contents.swift
@@ -1,104 +1,77 @@
-class Person_ {
-    let firstName: String
-    let lastName: String
-    var fullName: String {
-        return "\(firstName) \(lastName)"
-    }
-
-    init(firstName: String, lastName: String) {
+class Person1 {
+    var firstName : String
+    var lastName : String
+    var fullName : String { return "\(firstName) \(lastName)"}
+    
+    init(firstName : String, lastName : String) {
         self.firstName = firstName
         self.lastName = lastName
     }
-
+    
     func goForARun() {
         print("I love running!")
     }
 }
 
-let jim = Person_(firstName: "Jimbo", lastName: "Guiseppe")
+let jim = Person1(firstName: "Jimbo", lastName: "Giuseppe")
 print(jim.fullName)
 jim.goForARun()
 
 
 struct Person {
-    let firstName: String
-    let lastName: String
-    var fullName: String {
-        return "\(firstName) \(lastName)"
-    }
-
+    var firstName : String
+    var lastName : String
+    var fullName : String { return "\(firstName) \(lastName)" }
+    
     func goForARun() {
-        print("I love running!")
+        print ("I love running!")
     }
 }
 
-let jim2 = Person(firstName: "Jimbo", lastName: "Guiseppe")
-print(jim.fullName)
+let jim2 = Person(firstName: "Jimbo", lastName: "Giuseppe")
+print(jim2.fullName)
 jim.goForARun()
 
-
-
-class PersonReference {
-    var firstName: String
-    var lastName: String
-    var fullName: String {
-        return "\(firstName) \(lastName)"
-    }
-
-    init(firstName: String, lastName: String) {
-        self.firstName = firstName
-        self.lastName = lastName
-    }
-}
-
-var person1 = PersonReference(firstName: "Luke", lastName: "Skywalker")
+var person1 = Person1(firstName: "Luke", lastName: "Skywalker")
 var person2 = person1
+
 print(person1.fullName)
 print(person2.fullName)
+
 person1.firstName = "Han"
 person1.lastName = "Solo"
+
 print(person1.fullName)
 print(person2.fullName)
 
+var hero1 = Person(firstName: "Luke", lastName: "Skywalker")
+var hero2 = hero1
 
+print(hero1.fullName)
+print(hero2.fullName)
 
-struct PersonValue {
-    var firstName: String
-    var lastName: String
-    var fullName: String {
-        return "\(firstName) \(lastName)"
+hero1.firstName = "Han"
+hero1.lastName = "Solo"
+
+print(hero1.fullName)
+print(hero2.fullName)
+
+class Mug1 {
+    var amountOfCoffee: Double = 0.0
+    
+    func fillMug(mug: Mug1) {
+        mug.amountOfCoffee = 10.0
     }
 }
 
-var hero1 = PersonValue(firstName: "Luke", lastName: "Skywalker")
-var hero2 = hero1
-print(hero1.fullName)
-print(hero2.fullName)
-hero1.firstName = "Han"
-hero1.lastName = "Solo"
-print(hero1.fullName)
-print(hero2.fullName)
-
-
-
-class Mug_ {
-    var amountOfCoffee: Double = 0.0
-}
-
-func fillMug_(mug: Mug_) {
-    mug.amountOfCoffee = 10.0
-}
-
-let myMug_ = Mug_()
-print(myMug_.amountOfCoffee)
-fillMug_(myMug_)
-print(myMug_.amountOfCoffee)
-
-
+let myMug = Mug1()
+print(myMug.amountOfCoffee)
+myMug.fillMug(mug: myMug)
+print(myMug.amountOfCoffee)
 
 struct Mug {
     var amountOfCoffee: Double = 0.0
-
+    
     mutating func fillMug() {
         amountOfCoffee = 10.0
     }
@@ -108,3 +81,8 @@ var myMug1 = Mug()
 print(myMug1.amountOfCoffee)
 myMug1.fillMug()
 print(myMug1.amountOfCoffee)
+
+let myMug2 = Mug()
+myMug2.fillMug()
+
+
